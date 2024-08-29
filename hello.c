16d391e426989236b0079cea5514a3426cccaca3
twilliamson6 <twilliamson6@huskers.unl.edu>
2024-08-29 09:15:55 -0600

    initial commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+
+
+
+
+    int main(int argc, char **argv) {
+        printf("Hello World, I'm Trey Williamson!\n");
+        printf("My Major is Electrical Engerneering!\n);
+
+    return 0;
+    }

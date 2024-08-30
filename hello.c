900bfacbcf725e44905babb5d2fe6988fdeb3734
twilliamson6 <twilliamson6@huskers.unl.edu>
2024-08-30 08:08:51 -0600

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
-
+#include <stdio.h>
 
 
 
     int main(int argc, char **argv) {
         printf("Hello World, I'm Trey Williamson!\n");
-        printf("My Major is Electrical Engerneering!\n);
+        printf("My Major is Electrical Engerneering!\n");
 
     return 0;
     }
